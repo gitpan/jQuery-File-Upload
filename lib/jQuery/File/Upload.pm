751f8c15b72ca2818c92345a5f373717e951f800
Adam Hopkins <srchulo@cpan.org>
2013-04-17 09:38:37 +0000

    Import of SRCHULO/jQuery-File-Upload-0.15 from CPAN.
    gitpan-cpan-distribution: jQuery-File-Upload
    gitpan-cpan-version:      0.15
    gitpan-cpan-path:         SRCHULO/jQuery-File-Upload-0.15.tar.gz
    gitpan-cpan-author:       SRCHULO
    gitpan-cpan-maturity:     released

--- lib/jQuery/File/Upload.pm
+++ lib/jQuery/File/Upload.pm
@@ -16,7 +16,7 @@ use URI;
 #use LWP::UserAgent;
 #use LWP::Protocol::https;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 my %errors =  (
 	'_validate_max_file_size' => 'File is too big',
@@ -656,6 +656,7 @@ sub is_image { shift->{is_image} }
 sub print_response { 
 	my $self = shift;
 
+	my $content_type = 'text/plain';
 	if(defined $self->ctx) { 
 
 		#thanks to Lukas Rampa for this suggestion
